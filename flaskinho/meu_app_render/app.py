c2c69d8569345c44663c9b61950b1cd6c7d34ae7
Lemuel Duarte <lemuelduartep@gmail.com>
2025-12-23 17:18:30 -0300

    add Flaskinho

---
+++ flaskinho/meu_app_render/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def exibir_home():
+    return render_template("home.html")
+
+@app.route("/contato")
+def exibir_contato():
+    return render_template("contato.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
